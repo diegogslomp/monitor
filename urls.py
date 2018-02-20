557356f92d047df664dbd989e9fd7531148eccca
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-02-20 19:33:51 -0300

    added app_name to urls.py

--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'monitor'
+
 urlpatterns = [
                 url(r'^$', views.IndexView.as_view(), name='index'),
                 url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
