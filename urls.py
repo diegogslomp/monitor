4a349a42ef96f5e48ca84b4a0d6149ede415ab2d
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-02-21 13:51:24 -0300

    django2.0 path urls

--- urls.py
+++ urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import url
-
+from django.urls import path
 from . import views
 
 app_name = 'monitor'
-
 urlpatterns = [
-                url(r'^$', views.IndexView.as_view(), name='index'),
-                url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+                path('', views.IndexView.as_view(), name='index'),
+                path('<int:pk>/', views.DetailView.as_view(), name='detail'),
               ]
