6e95985b8aa2251dadb3458a7d6c10d967d2aca0
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-08-09 14:32:39 -0300

    comments removed from

--- views.py
+++ views.py
@@ -1,10 +1,12 @@
 from django.views.generic import DetailView, ListView, TemplateView
 from monitor.models import Host, Port, PortLog
 
+
 class IndexView(TemplateView):
 
     template_name = 'monitor/index.html'
     
+
 class HostListView(ListView):
 
     template_name = 'monitor/host_list.html'
@@ -13,9 +15,11 @@ class HostListView(ListView):
     def get_queryset(self):
         return Host.objects.all().order_by('-status', '-last_status_change')
 
+
 class PortView(TemplateView):
 
     template_name = 'monitor/ports.html'
+
 
 class PortListView(ListView):
 
@@ -23,14 +27,10 @@ class PortListView(ListView):
     context_object_name = 'port_list'
 
     def get_queryset(self):
-        return Port.objects.filter(counter_status__gt=2, error_counter__gt=50).order_by('-counter_last_change', '-counter_status', 'error_counter')
+        return Port.objects.filter(counter_status__gt=2, error_counter__gt=50)
+                   .order_by('-counter_last_change', '-counter_status', 'error_counter')
 
-""" class DetailView(DetailView):
 
-    template_name = 'monitor/detail.html'
-    model = Host
-
- """
 class DetailView(TemplateView):
 
     template_name = 'monitor/detail.html'
