fc082073da0c8a35c918b58187652e613cb7088f
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-10-19 12:06:08 -0300

    added first tests

--- tests.py
+++ tests.py
@@ -1,3 +1,25 @@
+import datetime
+from django.utils import timezone
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Host
+
+class HostModelTest(TestCase):
+    def test_host_status(self):
+        """
+        Test a "pingable" host
+        """
+        active_host = Host(name='local', ipv4='127.0.0.1')
+        active_host.check_and_update_host()
+        self.assertEqual(Host.SUCCESS, active_host.status)
+
+class HostListViewTests(TestCase):
+    def test_no_host_list(self):
+        """
+        If no hosts exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('monitor:host_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No hosts are available.")
+        self.assertQuerysetEqual(response.context['host_list'], [])
