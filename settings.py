d631951cf227b0c164c9ae55efd635b711759219
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-08-09 08:28:28 -0300

    wait 0.5 seconds instead 1

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 # Days to change the status from red (danger) to yellow (warning)
 DAYS_FROM_DANGER_TO_WARNING = 5
 # Time to wait after each host check in seconds (0..)
-WAIT_FOR_NEXT = 1
+WAIT_FOR_NEXT = 0.5
 # Max log lines for each host
 MAX_LOG_LINES = 20
 # User for telnet login
