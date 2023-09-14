ffcedff500125d35aedd01754213b9229dc69fbc
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-09-14 20:51:45 -0300

    Use Asyncio to handle daemon loops

--- management/commands/monitord.py
+++ management/commands/monitord.py
@@ -1,20 +1,50 @@
 from django.core.management.base import BaseCommand
-from monitor.models import Host
 from monitor.tasks.host import check_and_update
+from monitor.models import Host
+
+from asyncio import Queue
+from time import sleep
+import asyncio
 import logging
+
+
+def task(host: Host) -> None:
+    logging.debug(f"Working on {host}")
+    try:
+        check_and_update(host)
+        sleep(1)
+    except Exception as e:
+        logging.warning(f"Error checking {host}")
+        logging.debug(e)
+    finally:
+        logging.debug(f"Finished {host}")
+
+
+async def worker(q: Queue) -> None:
+    while True:
+        item = await q.get()
+        task(item)
+        q.task_done()
+
+
+async def main() -> None:
+    q = Queue()
+
+    asyncio.create_task(worker(q))
+
+    while True:
+        for host in Host.objects.all():
+            q.put_nowait(host)
+
+        # Block until all tasks are done.
+        await q.join()
+        logging.debug("All work completed")
 
 
 class Command(BaseCommand):
     args = ""
-    logger = logging.getLogger(__name__)
     help = "Monitor Daemon for Monitor hosts"
 
     def handle(self, *args, **options):
-        self.logger.info("Monitord started")
-        while True:
-            for host in Host.objects.all():
-                try:
-                    check_and_update(host)
-                except Exception as e:
-                    self.logger.warning(f"Error checking {host}")
-                    self.logger.debug(e)
+        logging.info("Monitord started")
+        asyncio.run(main())
--- management/commands/portcounterd.py
+++ management/commands/portcounterd.py
@@ -1,26 +1,53 @@
 from django.core.management.base import BaseCommand
 from monitor.models import Host, Status
 from monitor.tasks import telnet
-import time
+
+from asyncio import Queue
+from time import sleep
+import asyncio
 import logging
+
+
+def task(host: Host):
+    logging.debug(f"Working on {host}")
+    try:
+        if host.status == Status.SUCCESS:
+            telnet.telnet_port_counters(host)
+            sleep(1)
+            telnet.telnet_switch_manager(host)
+            sleep(1)
+    except Exception as e:
+        logging.warning(f"Error checking {host} port counters/manager")
+        logging.debug(e)
+    finally:
+        logging.debug(f"Finished {host}")
+
+
+async def worker(q: Queue) -> None:
+    while True:
+        item = await q.get()
+        task(item)
+        q.task_done()
+
+
+async def main() -> None:
+    q = Queue()
+
+    asyncio.create_task(worker(q))
+
+    while True:
+        for host in Host.objects.all():
+            q.put_nowait(host)
+
+        # Block until all tasks are done.
+        await q.join()
+        logging.debug("All work completed")
 
 
 class Command(BaseCommand):
     args = ""
-    logger = logging.getLogger(__name__)
-    help = "Monitor Port Errors from Hosts"
+    help = "Monitor Daemon for Monitor hosts"
 
     def handle(self, *args, **options):
-        self.logger.info("Portcounterd started")
-        while True:
-            for host in Host.objects.all():
-                try:
-                    if host.status == Status.SUCCESS:
-                        telnet.telnet_port_counters(host)
-                        time.sleep(1)
-                        telnet.telnet_switch_manager(host)
-                        time.sleep(1)
-                except Exception as e:
-                    self.logger.warning(f"Error checking {host} port counters/manager")
-                    self.logger.debug(e)
-
+        logging.info("Portcounterd started")
+        asyncio.run(main())
