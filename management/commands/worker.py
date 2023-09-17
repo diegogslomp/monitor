f436a76a26563516f4dd77577f5e58167a39b801
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-09-17 05:23:38 -0300

    Worker for sync or async tasks

--- management/commands/worker.py
+++ management/commands/worker.py
@@ -1,39 +1,33 @@
 from django.db.models import Model
 from asyncio import Queue
 import asyncio
-import logging
-import time
+import inspect
 import os
 
 
-async def handle_queue_forever(q: Queue, model: Model):
+async def handle_queue_forever(q: Queue, model: Model) -> None:
     while True:
         for item in model.objects.all():
-            await q.put(item)
-
-        started_at = time.monotonic()
+            q.put_nowait(item)
         await q.join()
-        completed_at = time.monotonic() - started_at
-
-        logging.debug(f"Loop completed in {completed_at:.2f} seconds")
 
 
-async def worker(name: str, q: Queue, task) -> None:
+async def worker(q: Queue, task: callable) -> None:
     while True:
         item = await q.get()
-        logging.debug(f"{name} working on {item}")
-
-        loop = asyncio.get_event_loop()
-        await loop.run_in_executor(None, task, item)
-
+        if inspect.iscoroutinefunction(task):
+            await task(item)
+        else:
+            loop = asyncio.get_event_loop()
+            await loop.run_in_executor(None, task, item)
         q.task_done()
 
 
-async def main(model: Model, task) -> None:
+async def main(model: Model, task: callable) -> None:
     q = Queue()
-    workers = int(os.getenv("WORKERS", 2))
 
-    for i in range(workers):
-        asyncio.create_task(worker(f"Worker-{i+1}", q, task))
+    workers = int(os.getenv("WORKERS", 3))
+    for _ in range(workers):
+        asyncio.create_task(worker(q, task))
 
     await handle_queue_forever(q, model)
